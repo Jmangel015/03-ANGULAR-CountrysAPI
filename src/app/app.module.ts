2ed56c249d50e71e82b9d095e4d4f6caaeb6ffce
Jorge Miguel Angel <jangel@people-inmotion.com>
2021-04-20 17:03:59 -0500

    RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PaisModule, SharedModule],
+  imports: [BrowserModule, PaisModule, SharedModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
