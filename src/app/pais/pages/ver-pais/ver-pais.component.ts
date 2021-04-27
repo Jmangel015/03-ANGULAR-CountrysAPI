b8a3c46b6b7379ba71750cdfc29a486484c794b4
Jorge Miguel Angel <jangel@people-inmotion.com>
2021-04-26 21:55:51 -0500

    Ver pais Finish

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interfaces';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+  pais!: Country;
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(
+        switchMap(({ id }) => this.paisService.getPaisAlpha(id)),
+        tap(console.log)
+      )
+      .subscribe((pais) => {
+        this.pais = pais;
+      });
+    // this.activateRoute.params.subscribe(({ id }) => {
+    //   this.paisService.getPaisAlpha(id).subscribe((pais) => {
+    //     console.log(pais);
+    //   });
+    // });
   }
-
 }
